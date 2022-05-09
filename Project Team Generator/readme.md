2297014e7a52fa2dc75400fbd25cb446db9ec386
Onur <onurbelek@outlook.com>
2022-05-09 23:33:49 +0100

    added readme.md

---
+++ Project Team Generator/readme.md
@@ -0,0 +1,17 @@
+# Lap 3 Debug Assignment
+Hello! Welcome to Onur's Lap 3 Debug Assignment! Thanks Dan for for popping by :)
+
+## How to Run the App
+* ```npm install``` - install all the node packages
+* ```npm run dev``` - now fire it up! 
+
+## changeLog
+Below are my magnificent, ground-breaking changes
+
+* 
+
+## InitialBugs (ew)
+*  
+
+## Wins & Challenges
+All I do is win win no matter what
--- Project Team Generator/uniqueTeam.js
+++ Project Team Generator/uniqueTeam.js
@@ -1,39 +1,38 @@
-const onur = ["Aafthab",
-    "Naomi",
+const diren = [
+    "Aafthab",
     "Trina",
     "Yusra",
     "Andrew",
     "Dan (student dan)",
+    "Robyn",
     "Amarachi",
-    "Prishal",
+    "Khari",
+    "Libby",
     "Thayaan",
     "Albert",
     "Alfie",
-    "David",
-    "Doreen"
+    "David"
 ]
 
 const teamGenerator = (name) => {
-    let arr = name // I want a COPY of the input array
+    let arr = name 
 
-    let team = [] // I want an empty array to populate with new random members
+    let team = [] 
 
-    for (let i = 0; i < 3; i ++) { // I want to loop this code below 3 times 
-
-        let random = Math.floor(Math.random() * arr.length); // selects a random INDEX for the array 
-        let member = arr[random]; // uses the random index to get a random member 
-        console.log(member)
-        team.push(member)  // push this random member to the 
-
-        let takeOutMemberFromOriginalArray = arr.slice(0, random).concat(arr.slice(random + 1)); // make new array WITHOUT the selected person in it
-        arr = takeOutMemberFromOriginalArray; // reassign the copy array to the array WITHOUT the selected person
-
-        console.log(arr)
-         
+    for (let i = 1; i < 5; i ++) {
+        for (let j = 1; j < 4; j ++) { 
+            let random = Math.floor(Math.random() * arr.length); 
+            let member = arr[random]; 
+            team.push([member, i, j])  
+    
+            let takeOutMemberFromOriginalArray = arr.slice(0, random).concat(arr.slice(random + 1)); 
+            arr = takeOutMemberFromOriginalArray; 
+        }
     }
 
+    console.log(arr)
     return team
 }
 
-console.log(teamGenerator(onur))
+console.log(teamGenerator(diren))
 
